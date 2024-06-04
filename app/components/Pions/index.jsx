3206d94b3a28cd713414beb26c3353eea1795976
Djessy Coiffe <djessy.coiffe@gmail.com>
2024-06-04 12:01:10 +0200

    Suspense pour searchParams

--- app/components/Pions/index.jsx
+++ app/components/Pions/index.jsx
@@ -2,6 +2,7 @@
 
 import { useSearchParams } from 'next/navigation'
 import Image from "next/image"
+import { Suspense } from 'react'
 
 const basePath = '/img/pions/pion'
 
@@ -10,20 +11,25 @@ const Pions = () => {
   const animals = searchParams.get('animals')
   const names = searchParams.get('names')
 
-  // Si animals est une chaîne, la convertir en tableau
   const pieces = animals ? animals.split(',') : []
   const playerNames = names ? names.split(',') : []
 
   return (
-    <div className="flex">
+    <div className="flex flex-col items-center">
       {pieces.map((piece, index) => (
-        <div key={index} className='flex flex-col items-center'>
+        <div key={index} className="flex flex-col items-center">
           <Image src={`${basePath}${piece}.png`} className='ml-4 rotate-90' alt={`Pièce de ${piece}`} width={100} height={100} priority />
-          <p className='mt-8 ml-4 text-2xl font-bRiver'>{playerNames[index]}</p>
+          <p className="mt-2">{playerNames[index]}</p>
         </div>
       ))}
     </div>
   )
 }
 
-export default Pions
+const PionsWithSuspense = () => (
+  <Suspense fallback={<div>Loading...</div>}>
+    <Pions />
+  </Suspense>
+)
+
+export default PionsWithSuspense
