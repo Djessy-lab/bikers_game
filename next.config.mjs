111879dd4314f312df439a0fa049ca39f056438d
Djessy Coiffe <djessy.coiffe@gmail.com>
2024-06-07 19:12:47 +0200

    fix ok

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+import path from 'path';
+
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve('./'),
+    };
+
+    return config;
+  },
+};
 
 export default nextConfig;
